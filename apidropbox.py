cc768456f164346d6a95d3f40bd900ffb2394ed9
rnov <rsmarinov@gmail.com>
2016-03-23 13:42:33 +0100

    nuevas funciones y mejoras de las mismas

--- apidropbox.py
+++ apidropbox.py
@@ -2,10 +2,9 @@ import dropbox
 import os
 from multiprocessing import Pool, TimeoutError
 
-# local paths to work (Test)
+# local paths (Test)
 local_path1 = '/home/rnov/tfg/dropboxApi/xytFiles'
-local_path2 = '/home/rnov/tfg/dropboxApi/xytFiles2/'
-local_path3 = '/home/rnov/tfg/dropboxApi/xytFiles3/'
+local_path2 = '/home/rnov/tfg/dropboxApi/local_path/'
 
 # make a connection to dropBox. OAuth flow authentication
 dbx = dropbox.dropbox.Dropbox('rrFPgdcaJP8AAAAAAAAAHbe0eiQ3StEF2OGqWTp1DM90UeAXMEzMyZCBLlOezKbs')
@@ -19,7 +18,7 @@ def upload_file(local_path, dropbox_path):
     uploads single file from a given local path to given dropbox path
     :param local_path: str source path
     :param dropbox_path: destiny dropbox path
-    :return:
+    :return: None
     """
     try:
         dbx.files_upload(local_path, dropbox_path, overwrite)  # dropbox_path = /xytFiles/
@@ -30,15 +29,16 @@ def upload_file(local_path, dropbox_path):
 
 def upload_files(local_path, dropbox_path):
     """
-    upload files from given local path to given dropbox path
+    uploads files from given local path to given dropbox path
     :param local_path: str local source path whose files will be uploaded
     :param dropbox_path: str dropbox's path where the file will be uploaded
-    :return:
+    :return: None
     """
     try:
         listFiles = os.listdir(local_path)
         for xytfile in listFiles:
-            fingptr = open(local_path+'/{0}'.format(xytfile))
+            fingptr = open(local_path+'{0}'.format(xytfile))
+            print fingptr
             dbx.files_upload(fingptr, dropbox_path+xytfile, overwrite)  # dropbox_path = /xytFiles/
             fingptr.close()
     except dropbox.exceptions.ApiError:
@@ -47,11 +47,46 @@ def upload_files(local_path, dropbox_path):
         print 'Unable to open file {0}: {1}'.format(e.errno, e.strerror)
 
 
+def aux_upload_parallel(xytfile, local_path, dropbox_path):
+    """
+    auxiliary function that helps upload_files_parallel() function to upload files in each process
+    :param xytfile: file name
+    :param local_path: str local source path whose files will be uploaded
+    :param dropbox_path: str dropbox's path where the file will be uploaded
+    :return: None
+    """
+    try:
+        fingptr = open(local_path+'{0}'.format(xytfile))
+        dbx.files_upload(fingptr, dropbox_path, overwrite)  # dropbox_path = /xytFiles/name.xyt
+        fingptr.close()
+    except IOError as e:
+        print 'Unable to open file {0}: {1}'.format(e.errno, e.strerror)
+    except dropbox.exceptions.ApiError:
+        print 'failed uploading files'
+
+
+def upload_files_parallel(local_path, dropbox_path, num_processes=20):
+    """
+    uploads files from given local path to given dropbox path. Multiprocess function
+    :param local_path: str local source path whose files will be uploaded
+    :param dropbox_path: str dropbox's path where the file will be uploaded
+    :param num_processes: number of processes to be launched
+    :return: None
+    """
+    pool = Pool(processes=num_processes)
+    list_files = os.listdir(local_path)  # file's names in the given path
+    multiple_results = [pool.apply_async(aux_upload_parallel, (xyt_name, local_path, dropbox_path+xyt_name))
+                        for xyt_name in list_files]
+    #print multiple_results
+    for res in multiple_results:
+        res.get()  # timeout=3
+
+
 def delete_content(dropbox_path):
     """
     deletes given file/folder from dropbox
     :param dropbox_path: str dropbox's (file/folder) path
-    :return:
+    :return: None
     """
     try:
         dbx.files_delete(dropbox_path)
@@ -61,10 +96,10 @@ def delete_content(dropbox_path):
 
 def retrieve_file(dropbox_path, local_path):
     """
-    download a single file
+    downloads a single file
     :param local_path: str path/file.format'
     :param dropbox_path: str '/folder/file.format'
-    :return:
+    :return: None
     """
     try:
         dbx.files_download_to_file(local_path, dropbox_path)
@@ -74,30 +109,29 @@ def retrieve_file(dropbox_path, local_path):
 
 def retrieve_folder(local_path, dropbox_path):
     """
-    download files from a given dropbox's folder to a given local path
+    downloads files from a given dropbox's folder to a given local path
     :param local_path: str path to be saved the files
     :param dropbox_path: str dropbox path
-    :return:
+    :return: None
     """
     folder = dbx.files_list_folder(dropbox_path)
     for k in folder.entries:
         retrieve_file(k.path_lower, local_path+k.name)
 
 
-def retrieve_file_parallel(files_name, path_files):
-    retrieve_file(path_files, local_path3+files_name)
-
-
-def retrieve_folder_parallel(dropbox_path):
+def retrieve_folder_parallel(dropbox_path, local_path, num_processes=15):
     """
-    download files from a given dropbox's folder to a given local path. Multiprocess function
+    downloads files from given dropbox's folder to a given local path. Multiprocess function
     :param dropbox_path: str dropbox path
-    :return:
+    :param local_path: str
+    :param num_processes int
+    :return: None
     """
     folder = dbx.files_list_folder(dropbox_path)
-    pool = Pool(processes=15,)
-    filesInfo = [(i.name, i.path_lower) for i in folder.entries]  # filesInfo: (filename, filepath)
-    multiple_results = [pool.apply_async(retrieve_file_parallel, (k[0], k[1],)) for k in filesInfo]
+    pool = Pool(processes=num_processes,)
+    files_info = [(i.name, i.path_lower) for i in folder.entries]  # filesInfo: (filename, filepath)
+    #  print files_info
+    multiple_results = [pool.apply_async(retrieve_file, (k[1], local_path+k[0])) for k in files_info]
     # print [res.get(timeout=1) for res in multiple_results]
     for res in multiple_results:
         res.get()  # timeout=3
