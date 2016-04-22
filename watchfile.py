7c9e9f90489391273b735228b8d55ed52da9c9d1
rnov <rsmarinov@gmail.com>
2016-04-22 03:50:49 +0200

    se ha adaptado a la estructura final, se han pulido algunos detalles

--- watchfile.py
+++ watchfile.py
@@ -1,26 +1,24 @@
 import sys
 import os
-import subprocess
-from multiprocessing import Process, Pipe, Queue
+from multiprocessing import Process, Queue
 import time
 import logging
 import linecache
 from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler
-from watchdog.events import FileSystemEvent
 import download_chunks as dw
 
+# YALM
 queue = Queue(2)
-queue_timeout = 20
-#f_path = 'docker/local_path/'
-local_path = 'docker/local_path/'  # sobra para yalm ahora se lo generamos
+queue_timeout = 10
 log_path = 'my_log.log'
 OAuth_token = 'rrFPgdcaJP8AAAAAAAAAHbe0eiQ3StEF2OGqWTp1DM90UeAXMEzMyZCBLlOezKbs'
 OAuth_token_2 = ''
 OAuth_token_3 = ''
 min_value = 30
 name_lis = 'listXyt'
-#watch_path = 'docker/watchdog_listening/'
+enc_format = '.aes'
+###
 #local_path = 'docker/local_path/'  # local_path == f_path it is the work path ...
 
 slash = '/'
@@ -54,26 +52,25 @@ def treat_txt_file(path_tfile):
 
 def treat_img(path_file):
     '''
-    discover whether a directory for a img exists or not, if exists returns its name, False otherwise.
+    discover whether a directory exists or not (used for incoming images), if exists returns its name, False otherwise.
     :param path_file: str, dir name
     :return: work dir name or False
     '''
     dir_name = os.path.basename(path_file)
     dir_name = dir_name[:dir_name.index('.')]
-    docker = 'docker/'  # docker/ sobra ya que estaran a la misma altura los .py
-    if os.path.exists(docker+dir_name):
+    if os.path.exists(dir_name):
         return dir_name
     return False
 
 
 def create_dir(t_res):
     '''
-    creates directories for a single operation
+    create directories for a single operation.
     :param t_res: tuple
     :return: False in case of error or path for success
     '''
     # make dir with the format '<id-termial>-<pin>'
-    dir_name = 'docker/{0}-{1}'.format(t_res[0], t_res[2])  # docker/ es temporal !!!! misma alt codigo
+    dir_name = '{0}-{1}'.format(t_res[0], t_res[2])
     local_cmp = '{0}/local_cmp/'.format(dir_name)
     try:
         os.mkdir(dir_name)  # create dir for terminal's request
@@ -82,9 +79,68 @@ def create_dir(t_res):
         logging.error('directory already exist')
         return False
     else:
-        return local_cmp
+        return dir_name, local_cmp
 
 
+class MySLoggingEventHandler3(LoggingEventHandler):
+
+    def on_created(self, event):
+        print 'file has been created {0}'.format(event.src_path)
+        file_format = event.src_path[event.src_path.rfind('.'):]  # finds the last point.
+
+        if file_format == '.txt':
+            # read data and return tuple or false
+            t_res = treat_txt_file(event.src_path)
+            # in case of success: charge or register
+            if t_res and t_res[1] == 'charge':
+                dir_name, local_cmp = create_dir(t_res)
+                if local_cmp:
+                    p = Process(target=dw.download_tar, args=(local_cmp, queue, OAuth_token, name_lis, enc_format,))
+                    p.start()
+                    # hacer o no hacer join para que lo espere, afectara queue_timeout, con join no hace falta queue
+                    #print 'at the end should send a signal to start comparing'
+
+                    p = Process(target=dw.compare, args=(dir_name, local_cmp, queue, queue_timeout, min_value,))
+                    p.start()
+            elif t_res:  # register
+                print 'to do register'
+
+            os.remove(event.src_path)  # always delete .txt
+
+        elif file_format == ('.png' or '.jpg'):
+            dst_path = treat_img(event.src_path)
+            if dst_path:  # the folder exist, proceed...
+                p = Process(target=dw.extract_mindtct, args=(event.src_path, queue, dst_path))
+                p.start()
+            else:  # directory does not exists (timeout, txt to img)
+                os.remove(event.src_path)
+
+        else:  # other formats
+            os.remove(event.src_path)
+
+if __name__ == "__main__":
+    # logger
+    logging.basicConfig(filename=log_path, format='%(asctime)s - %(levelname)s: (%(module)s) %(funcName)s - '
+                                                        '%(message)s ', level=logging.ERROR)
+
+    path = sys.argv[1] if len(sys.argv) > 1 else '/home/rnov/tfg/dropboxApi/work_unit/watchdog_listening'  # watchdog
+    # listening path
+
+    event_handler = MySLoggingEventHandler3()  # LoggingEventHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
+
+
+# about 12 MB of memory
+
+'''
 class MySLoggingEventHandler2(LoggingEventHandler):
 
     def on_created(self, event):
@@ -110,63 +166,7 @@ class MySLoggingEventHandler2(LoggingEventHandler):
 
     #def on_deleted(self, event):
         #print 'file has been deleted {0}'.format(event.src_path)
-
-
-class MySLoggingEventHandler3(LoggingEventHandler):
-
-    def on_created(self, event):
-        print 'file has been created {0}'.format(event.src_path)
-        file_format = event.src_path[event.src_path.rfind('.'):]  # finds the last point.
-
-        if file_format == '.txt':
-            # read data and return tuple or false
-            t_res = treat_txt_file(event.src_path)
-            # in case of success: charge or register
-            if t_res and t_res[1] == 'charge':
-                local_cmp = create_dir(t_res)
-                if local_cmp:
-                    p = Process(target=dw.download_tar, args=(local_cmp, queue, OAuth_token, name_lis,))  # download_chunks download_tar
-                    p.start()
-                    # hacer o no hacer join para que lo espere, afectara queue_timeout, con join no hace falta queue
-                    #print 'at the end should send a signal to start comparing'
-                    p = Process(target=dw.compare, args=(event.src_path, local_cmp, queue, queue_timeout, min_value,))
-                    p.start()
-            elif t_res and t_res[1] == 'register':
-                print 'to do register'
-
-            os.remove(event.src_path)  # always deletes .txt
-        elif file_format == ('.png' or '.jpg'):
-            dst_path = treat_img(event.src_path)
-            if dst_path:  # the folder exist, proceed
-                p = Process(target=dw.extract_mindtct, args=(event.src_path, queue, dst_path))  #  falta un arg donde guardar
-                p.start()
-            else:
-                os.remove(event.src_path)
-        else:
-            os.remove(event.src_path)
-
-if __name__ == "__main__":
-    # logger
-    logging.basicConfig(filename=log_path, format='%(asctime)s - %(levelname)s: (%(module)s) %(funcName)s - '
-                                                        '%(message)s ', level=logging.ERROR)
-
-    path = sys.argv[1] if len(sys.argv) > 1 else '/home/rnov/tfg/dropboxApi/docker/watchdog_listening'  # watchdog
-    # listening path
-
-    event_handler = MySLoggingEventHandler3()  # LoggingEventHandler()
-    observer = Observer()
-    observer.schedule(event_handler, path, recursive=True)
-    observer.start()
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        observer.stop()
-    observer.join()
-
-
-# about 12 MB of memory
-
+'''
 
     #logging.basicConfig(level=logging.INFO,
     #                    format='%(asctime)s - %(message)s',
