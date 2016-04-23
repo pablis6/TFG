76e2a327b22ee7ac7c1304fd902b28151b4d3703
rnov <rsmarinov@gmail.com>
2016-04-23 19:56:34 +0200

    se ha rediseñado la comunicación entre procesos, ahora hay una cola por job en vez de una global permitiendo asi que se puedan tratar varios trabajos de forma simultanea, para eso también se ha cambiado código dentro del handler del watchdog, se ha probado y funciona

--- watchfile.py
+++ watchfile.py
@@ -9,7 +9,6 @@ from watchdog.events import LoggingEventHandler
 import download_chunks as dw
 
 # YALM
-queue = Queue(2)
 queue_timeout = 10
 log_path = 'my_log.log'
 OAuth_token = 'rrFPgdcaJP8AAAAAAAAAHbe0eiQ3StEF2OGqWTp1DM90UeAXMEzMyZCBLlOezKbs'
@@ -21,6 +20,8 @@ enc_format = '.aes'
 ###
 #local_path = 'docker/local_path/'  # local_path == f_path it is the work path ...
 
+#queue = Queue(2)
+dict_q = {}
 slash = '/'
 
 
@@ -30,6 +31,9 @@ def treat_txt_file(path_tfile):
     :param path_tfile: str, path to the text file
     :return: tuple or False in case of error
     '''
+#if os.path.exists(os.path.basename(path_tfile)):  # in case the dir still exists, imposible siendo la misma terminal
+#    return False  # mejor que no este aqui por depende del formato nomber de txt , pierde flexibilidad
+
     id_terminal = linecache.getline(path_tfile, 1).rstrip()  # removes '\n'
     t_op = linecache.getline(path_tfile, 2).rstrip()
     pin = linecache.getline(path_tfile, 3).rstrip()
@@ -60,6 +64,8 @@ def treat_img(path_file):
     dir_name = dir_name[:dir_name.index('.')]
     if os.path.exists(dir_name):
         return dir_name
+
+    # logging.warning('orphan image ') # warning an image has arrived but there isn't an folder, reason timeout or error
     return False
 
 
@@ -73,7 +79,7 @@ def create_dir(t_res):
     dir_name = '{0}-{1}'.format(t_res[0], t_res[2])
     local_cmp = '{0}/local_cmp/'.format(dir_name)
     try:
-        os.mkdir(dir_name)  # create dir for terminal's request
+        os.mkdir(dir_name)  # create dir for terminal's request, raise error if exists
         os.mkdir(local_cmp)  # create sub-dir
     except OSError:
         logging.error('directory already exist')
@@ -82,13 +88,82 @@ def create_dir(t_res):
         return dir_name, local_cmp
 
 
+def queue_to_dict(dir_name, work_q):
+    dict_q[dir_name] = work_q
+
+
+class MySLoggingEventHandler4(LoggingEventHandler):
+
+    def on_created(self, event):
+        print 'file has been created {0}'.format(event.src_path)
+        file_format = event.src_path[event.src_path.rfind('.'):]  # finds the last point.
+        if file_format == '.txt':
+
+            # read data and return tuple or false
+            t_res = treat_txt_file(event.src_path)
+            # in case of success: charge or register
+            if t_res and t_res[1] == 'charge':
+                paths = create_dir(t_res)  # dir_name -> <id_terminal>-<pin
+                if paths:  # paths = (dir_name, local_cmp)
+                    queue = Queue(2)  # a queue per job
+                    dict_q[paths[0]] = queue  # saves queue dir to dir with dir name as key
+                    p = Process(target=dw.download_tar, args=(paths[1], queue, OAuth_token, name_lis, enc_format,))
+                    p.start()
+                    # hacer o no hacer join para que lo espere, afectara queue_timeout, con join no hace falta queue
+
+                    p = Process(target=dw.compare, args=(paths[0], paths[1], queue, queue_timeout, min_value,))
+                    p.start()
+            elif t_res:  # register
+                print 'to do register'
+
+            os.remove(event.src_path)  # always delete .txt
+
+        elif file_format == ('.png' or '.jpg'):
+            dir_name = treat_img(event.src_path)  # dir_name -> <id_terminal>-<pin>
+            if dir_name:  # the folder exist, proceed...
+                # get queue object form the dictionary ...
+                queue = dict_q.pop(dir_name, False)
+                if queue:
+                    p = Process(target=dw.extract_mindtct, args=(event.src_path, queue, dir_name))
+                    p.start()
+            else:  # directory does not exists (timeout, txt to img), remove img
+                os.remove(event.src_path)
+
+        else:  # other files, formats
+            os.remove(event.src_path)
+
+if __name__ == "__main__":
+    # logger
+    logging.basicConfig(filename=log_path, format='%(asctime)s - %(levelname)s: (%(module)s) %(funcName)s - '
+                                                        '%(message)s ', level=logging.ERROR)
+
+    #logging.basicConfig(filename=log_path, format='%(asctime)s - %(levelname)s: (%(module)s) %(funcName)s - '
+    #                                                    '%(message)s ', level=logging.WARNING)
+
+    path = sys.argv[1] if len(sys.argv) > 1 else '/home/rnov/tfg/dropboxApi/work_unit/watchdog_listening'  # watchdog
+    # listening path
+
+    event_handler = MySLoggingEventHandler4()  # LoggingEventHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
+
+
+# about 12 MB of memory
+"""
 class MySLoggingEventHandler3(LoggingEventHandler):
 
     def on_created(self, event):
         print 'file has been created {0}'.format(event.src_path)
         file_format = event.src_path[event.src_path.rfind('.'):]  # finds the last point.
-
         if file_format == '.txt':
+
             # read data and return tuple or false
             t_res = treat_txt_file(event.src_path)
             # in case of success: charge or register
@@ -112,61 +187,12 @@ class MySLoggingEventHandler3(LoggingEventHandler):
             if dst_path:  # the folder exist, proceed...
                 p = Process(target=dw.extract_mindtct, args=(event.src_path, queue, dst_path))
                 p.start()
-            else:  # directory does not exists (timeout, txt to img)
+            else:  # directory does not exists (timeout, txt to img), remove img
                 os.remove(event.src_path)
 
-        else:  # other formats
+        else:  # other files, formats
             os.remove(event.src_path)
-
-if __name__ == "__main__":
-    # logger
-    logging.basicConfig(filename=log_path, format='%(asctime)s - %(levelname)s: (%(module)s) %(funcName)s - '
-                                                        '%(message)s ', level=logging.ERROR)
-
-    path = sys.argv[1] if len(sys.argv) > 1 else '/home/rnov/tfg/dropboxApi/work_unit/watchdog_listening'  # watchdog
-    # listening path
-
-    event_handler = MySLoggingEventHandler3()  # LoggingEventHandler()
-    observer = Observer()
-    observer.schedule(event_handler, path, recursive=True)
-    observer.start()
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        observer.stop()
-    observer.join()
-
-
-# about 12 MB of memory
-
-'''
-class MySLoggingEventHandler2(LoggingEventHandler):
-
-    def on_created(self, event):
-        print 'file has been created {0}'.format(event.src_path)
-        file_format = event.src_path[event.src_path.rfind('.'):]  # finds the last point.
-
-        if file_format == '.txt':
-            p = Process(target=dw.download_tar, args=(local_path, queue, OAuth_token, name_lis,))  # download_chunks download_tar
-            p.start()
-            #print 'at the end should send a signal to start comparing'
-        elif file_format == ('.png' or '.jpg'):
-            p = Process(target=dw.extract_mindtct, args=(event.src_path, queue,))  #  falta un arg donde guardar
-            p.start()
-            #print 'done with the image'
-        elif file_format == '.xyt':
-            print "here"
-            p = Process(target=dw.compare, args=(event.src_path, local_path, queue, queue_timeout, min_value,))
-            p.start()
-            #print 'send signal to compare... already got xyt'
-        else:
-            os.remove(event.src_path)
-            #print 'remove the file, it s not useful '
-
-    #def on_deleted(self, event):
-        #print 'file has been deleted {0}'.format(event.src_path)
-'''
+"""""
 
     #logging.basicConfig(level=logging.INFO,
     #                    format='%(asctime)s - %(message)s',
