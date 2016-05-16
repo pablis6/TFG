ba70f804a865393fa62d0297c9481eb8d3cde8e6
rnov <rsmarinov@gmail.com>
2016-05-16 13:24:39 +0200

    se han arreglado métodos para poder encryptar, utilizados para subir

--- encryption.py
+++ encryption.py
@@ -12,6 +12,7 @@ key_path_2 = 'secret2.key'
 split_path = os.getcwd()  # gets current working path, '/home/rnov/tfg/dropboxApi/work_unit'
 # variables que se utilizan a lo largo de todas las funciones, internar que se cargue su valor por YALMP
 
+binary = ''
 #
 # private aux. methods
 #
@@ -37,7 +38,7 @@ def __aux_wrapper_decrypt_chunks(aes_file, key_path_chunk, dst_path):
     :param dst_path: str destiny file path
     :return: None
     """
-    subprocess.call(['./aescrypt_32', '-d', '-k', key_path_chunk, dst_path+aes_file])
+    subprocess.call([binary, '-d', '-k', key_path_chunk, dst_path+aes_file])  # './aescrypt_32'
 
 
 def __aux_decrypt_file(chunk, files_path, dst_path, key_path, enc_format):
@@ -50,23 +51,35 @@ def __aux_decrypt_file(chunk, files_path, dst_path, key_path, enc_format):
         :param key_path: str key's path
         :return: None
         """
+        #print binary
         from_path = files_path+chunk+'*'
         to_path = dst_path+chunk
 
         cat_command = '{0} > {1}.xyt{2}'.format(from_path, to_path, enc_format)  # forms cat call
         subprocess.call('cat {0}'.format(cat_command), shell=True)  # merge the chucks from one file to a file.
-        subprocess.call('rm -f {0}{1}-*'.format(files_path, chunk), shell=True)  # removes single file's chucks
+
+
+        aux1 = subprocess.call('rm -f {0}{1}-*'.format(files_path, chunk), shell=True)  # removes single file's chucks
         single_file_path = to_path+'.xyt{0}'.format(enc_format)  # file_name
-        subprocess.call(['./aescrypt_32', '-d', '-k', key_path, single_file_path])  # decrypt single file
-        subprocess.call('rm -f {0}{1}*{2}'.format(dst_path, chunk, enc_format), shell=True)
+
+        aux2 = subprocess.call([binary, '-d', '-k', key_path, single_file_path])  # decrypt single file './aescrypt_32'
+        aux3 = subprocess.call('rm -f {0}{1}*{2}'.format(dst_path, chunk, enc_format), shell=True)
         # remove .aes remaining file
+        if chunk == 'robert':
+            print chunk
+            print cat_command
+            print aux1
+            print 'single_file_path {0}'.format(single_file_path)
+            print aux2
+            print aux3# """
+        #print single_file_path
 
 #
 # encryption methods
 #
 
 
-def encrypt_files(files_path, key_path, chunks_path, enc_format):
+def encrypt_files(files_path, key_path, chunks_path, enc_format, new_name=None):
     """
     encrypts .xyt files in a given directory with a given key, and splits them.
     :param files_path: str path to the directory
@@ -77,17 +90,22 @@ def encrypt_files(files_path, key_path, chunks_path, enc_format):
     list_files = os.listdir(files_path)
     for xytfile in list_files:
         if xytfile.rfind('.xyt') > 1:  # while the file has .xyt format
-            subprocess.call(['./aescrypt_32', '-e', '-k', key_path, files_path+xytfile])  # encrypt .xyt file
-            name = xytfile.rstrip('.xyt')
+            subprocess.call([binary, '-e', '-k', key_path, files_path+xytfile])  # encrypt .xyt file './aescrypt_32'
+            if new_name:  # tenemos un nombre que darle, registro
+                name = new_name
+            else:  # dejar el mismo nombre que trae la imagen, para subir gran cantidad de datos
+                name = xytfile.rstrip('.xyt')
             name += '-'  # name's format for the chunks
-            subprocess.call(['split', '-n', '3', files_path+xytfile+enc_format, name,
+            subprocess.call(['split', '-n', '3', files_path + xytfile + enc_format, chunks_path+name,
                              '--numeric-suffixes=2'])  # split the file in chunks (no deja en el mismo dir)
     # move the chunks back to the given path
-    list_files = os.listdir(split_path)
+    """
+    list_files = os.listdir(split_path)  # split_path -> current working path, where chunks are generated
     for chunks in list_files:
         if chunks.rfind('-0') > -1:
             #print chunks
-            subprocess.call(['mv', '-f', split_path+chunks, chunks_path+chunks])
+            print subprocess.call(['mv', '-f', split_path+chunks, chunks_path+chunks])
+    #"""
 
 
 def encrypt_chunks_tar(chunks_path, key_path, suffix):
@@ -101,7 +119,8 @@ def encrypt_chunks_tar(chunks_path, key_path, suffix):
     #suffix = '-0'  # needed suffix for the files to be processed
     for chunks_file in listFiles:
         if chunks_file.rfind(suffix) > 1:  # while the file has the suffix and it is not in the first position
-            subprocess.call(['./aescrypt_32', '-e', '-k', key_path, chunks_path+chunks_file])
+            subprocess.call([binary, '-e', '-k', key_path, chunks_path+chunks_file])  # './aescrypt_32'
+
 
 #
 # decryption methods
@@ -121,7 +140,7 @@ def decrypt_chunks(files_path, key_path_chunk, enc_format, dst_path=None):
     list_files = os.listdir(files_path)
     for aes_file in list_files:  # decrypt the chunks
         if aes_file.rfind(enc_format) > -1:  # while a file has the format .aes, decrypt it
-            subprocess.call(['./aescrypt_32', '-d', '-k', key_path_chunk, dst_path+aes_file])
+            subprocess.call([binary, '-d', '-k', key_path_chunk, dst_path+aes_file])  # './aescrypt_32'
 
     subprocess.call('rm -f {0}*{1}'.format(files_path, enc_format), shell=True)
     # remove all .aes files from files_path, for test in case same folder
@@ -184,14 +203,3 @@ def decrypt_to_file_parallel(files_path, key_path, enc_format, dst_path=None):
     for res in multiple_results:
         res.get()  # timeout=3
 
-
-def __aux_decrypt(chunks_path_1, enc_format):
-    """
-    auxiliary function that helps debugging decryption functions from encryption module
-    :param chunks_path_1:
-    :return:
-    """
-    decrypt_chunks_parallel(chunks_path_1, key_path_2, enc_format)
-    # decrypt_chunks(chunks_path_1, key_path_2)
-    # decrypt_to_file(chunks_path_1, key_path_1)
-    decrypt_to_file_parallel(chunks_path_1, key_path_1)
